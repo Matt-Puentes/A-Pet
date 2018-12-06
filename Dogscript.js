65bcbde8f9ac811d2c9c25970480df6b5e3d2ea4
Matthew Puentes <matt.puentes@gmail.com>
2018-12-05 19:01:14 -0500

    updated version, added model.

--- Dogscript.js
+++ Dogscript.js
@@ -1,38 +1,58 @@
-console.log("Game begin!")
-nextTick = -1
+function main(){
+    console.log("Game begin!")
+    nextTick = -1
+    var sceneEl = document.querySelector('a-scene');
+    var dog = sceneEl.querySelector('#dog');
 
-var sceneEl = document.querySelector('a-scene');
-var dog = sceneEl.querySelector('#dog');
+    loadDog()
+    addBounceAnimation()
 
+    window.addEventListener("keydown", function (event) {
+    	if(event.key == "r"){
+            dog.emit('bounce')
+        }
+    })
 
-addBounceAnimation(dog)
+    dog.addEventListener('click', function() {
+      dog.emit('bounce');
+    });
+}
 
-function addBounceAnimation(entity) {
+function loadDog(){
+    dog.setAttribute('scale', {x: .5, y: .5, z: .5}, true);
+}
+
+function addBounceAnimation() {
     var dogScale = dog.getAttribute('scale');
     var dogScale2 = Object.assign({}, dogScale);
-    dogScale2.y = dogScale2.y + 0.5
-    dogScale2.x = dogScale2.x + 0.10
-    dogScale2.z = dogScale2.z + 0.10
+    dogScale2.y = dogScale2.y * 1.5
+    dogScale2.x = dogScale2.x * 1.10
+    dogScale2.z = dogScale2.z * 1.10
     var dogPos = dog.getAttribute('position');
     var dogPos2 = Object.assign({}, dogPos);
-    dogPos2.y = dogPos2.y + 0.25
+    dogPos2.y = dogPos2.y + (dog.getAttribute('geometry').height * 0.5)
     var len = 500
 
-    entity.setAttribute('animation__scale1',{
+    console.log(dogScale)
+    console.log(dogScale2)
+    console.log(dogPos)
+    console.log(dogPos2)
+
+    dog.setAttribute('animation__scale1',{
         property:'scale',
         from: vec3tostr(dogScale),
         to: vec3tostr(dogScale2),
         startEvents: 'bounce',
         dur: len
     })
-    entity.setAttribute('animation__pos1',{
+    dog.setAttribute('animation__pos1',{
         property:'position',
         from: vec3tostr(dogPos),
         to: vec3tostr(dogPos2),
         startEvents: 'bounce',
         dur: len
     })
-    entity.setAttribute('animation__scale2',{
+    dog.setAttribute('animation__scale2',{
         property:'scale',
         from: vec3tostr(dogScale2),
         to: vec3tostr(dogScale),
@@ -40,7 +60,7 @@ function addBounceAnimation(entity) {
         delay: len,
         dur: len
     })
-    entity.setAttribute('animation__pos2',{
+    dog.setAttribute('animation__pos2',{
         property:'position',
         from: vec3tostr(dogPos2),
         to: vec3tostr(dogPos),
@@ -53,13 +73,3 @@ function addBounceAnimation(entity) {
 function vec3tostr(vec3){
     return vec3.x + " " + vec3.y + " " + vec3.z
 }
-
-window.addEventListener("keydown", function (event) {
-	if(event.key == "r"){
-        dog.emit('bounce')
-    }
-})
-
-dog.addEventListener('click', function() {
-  dog.emit('bounce');
-});
